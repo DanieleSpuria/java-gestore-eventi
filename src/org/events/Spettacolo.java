86e6e979c5ba91395e537a0d149b9ed007c8a531
DanieleSpuria <123416873+DanieleSpuria@users.noreply.github.com>
2023-09-25 17:26:12 +0200

    add Spettacolo

---
+++ src/org/events/Spettacolo.java
@@ -0,0 +1,20 @@
+package org.events;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Spettacolo extends Evento {
+	private BigDecimal prezzo;
+	
+	public Spettacolo(String titolo, LocalDate data, int postiTotali, BigDecimal prezzo) throws Exception {
+		super(titolo, data, postiTotali);
+		setPrezzo(prezzo);
+	}
+
+	public BigDecimal getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(BigDecimal prezzo) {
+		this.prezzo = prezzo;
+	}	
+}
--- src/org/gestore/Main.java
+++ src/org/gestore/Main.java
@@ -23,8 +23,7 @@ public class Main {
 		System.out.print("Inserire nome programma eventi: ");
 		String nomeProgramma = scan.nextLine();		
 		eventi = new ProgrammaEventi(nomeProgramma);
-		
-		
+			
 		// STAMPA PROGRAMMA EVENTI
 		boolean exit = false;
 		while(!exit) {
@@ -48,64 +47,63 @@ public class Main {
 			
 			switch (scelta) {
 				case 1: {
-					while (newEventoFlag) {
+					while (newEventoFlag) {	
+						// EVENTO
+						try {
+							System.out.println("\n°°Nuovo evento°°\n");
+							System.out.print("Inserire titolo: ");
+							String titolo = scan.nextLine();
+							System.out.print("Inserire data (yyyy-mm-dd): ");
+							LocalDate data = LocalDate.parse(scan.nextLine());
+							System.out.print("Inserire numero posti totali: ");
+							int postiTotali = Integer.valueOf(scan.nextLine());
 							
-							// EVENTO
+							evento = new Evento(titolo, data, postiTotali);			
+							eventi.addEvento(evento);
+							System.out.println("\n[ "+ evento + " ]\n");
+						} catch (Exception e) {
+							System.err.println(e.getMessage());
+						}
+						
+						// PRENOTAZIONI
+						boolean c = true;				
+						while(c) {
 							try {
-								System.out.println("\n°°Nuovo evento°°\n");
-								System.out.print("Inserire titolo: ");
-								String titolo = scan.nextLine();
-								System.out.print("Inserire data (yyyy-mm-dd): ");
-								LocalDate data = LocalDate.parse(scan.nextLine());
-								System.out.print("Inserire numero posti totali: ");
-								int postiTotali = Integer.valueOf(scan.nextLine());
-								
-								evento = new Evento(titolo, data, postiTotali);			
-								eventi.addEvento(evento);
-								System.out.println("\n[ "+ evento + " ]\n");
+								System.out.print("Vuoi aggiungere delle prenotazioni? (s/n): ");
+								resp = scan.nextLine();
+								if (resp.equalsIgnoreCase("s")) {
+									System.out.print("Quante prenotazioni vuoi aggiungere? ");
+									int preno = Integer.valueOf(scan.nextLine());
+									evento.prenota(preno);
+							}
+								c = false;
+								System.out.println("");
 							} catch (Exception e) {
 								System.err.println(e.getMessage());
 							}
+							System.out.println(evento.getPosti() + "\n");
 							
-							// AGGIUNGERE PRENOTAZIONI
-							boolean c = true;				
-							while(c) {
-								try {
-									System.out.print("Vuoi aggiungere delle prenotazioni? (s/n): ");
-									resp = scan.nextLine();
-									if (resp.equalsIgnoreCase("s")) {
-										System.out.print("Quante prenotazioni vuoi aggiungere? ");
-										int preno = Integer.valueOf(scan.nextLine());
-										evento.prenota(preno);
+						}
+						
+						// DISDIRE 
+						c = true;
+						while(c) {
+							try {
+								System.out.print("Vuoi disdire dei posti? (s/n): ");
+								resp = scan.nextLine();
+								if (resp.equalsIgnoreCase("s")) {
+									System.out.print("Quanti posti vuoi disdire? ");
+									int disdo = Integer.valueOf(scan.nextLine());
+									evento.disdici(disdo);
 								}
 									c = false;
-									System.out.println("");
-								} catch (Exception e) {
-									System.err.println(e.getMessage());
-								}
-								System.out.println(evento.getPosti() + "\n");
-								
+								System.out.println("");
+							} catch (Exception e) {
+								System.err.println(e.getMessage());
 							}
-							
-							// DISDIRE PRENOTAZIONI
-							c = true;
-							while(c) {
-								try {
-									System.out.print("Vuoi disdire dei posti? (s/n): ");
-									resp = scan.nextLine();
-									if (resp.equalsIgnoreCase("s")) {
-										System.out.print("Quanti posti vuoi disdire? ");
-										int disdo = Integer.valueOf(scan.nextLine());
-										evento.disdici(disdo);
-									}
-										c = false;
-									System.out.println("");
-								} catch (Exception e) {
-									System.err.println(e.getMessage());
-								}
-								System.out.println(evento.getPosti() + "\n");
-							}
-							break;
+							System.out.println(evento.getPosti() + "\n");
+						}
+						break;
 					}
 					break;
 				}
